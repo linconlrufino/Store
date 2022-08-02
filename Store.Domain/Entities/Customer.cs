155bb8d0e68234d6049c4c009f942bc2a9d7838d
Lincoml Rufino <linconldasilvarufino@gmail.com>
2022-08-01 22:15:04 -0300

    Add project files.

---
+++ Store.Domain/Entities/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace Store.Domain.Entities;
+
+public class Customer
+{
+    public Customer(string name, string emai)
+    {
+        Name = name;
+        Emai = emai;
+    }
+
+    public string Name { get; private set; }
+    public string Emai { get; private set; }
+}
