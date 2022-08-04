431d78efd345ed4f79220efce3d485e1db6c3708
Lincoml Rufino <linconldasilvarufino@gmail.com>
2022-08-03 22:28:07 -0300

    Discount entity

---
+++ Store.Domain/Entities/Discount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Domain.Entities;
+
+public class Discount : Entity
+{
+    public Discount(decimal amount, DateTime expireDate)
+    {
+        Amount = amount;
+        ExpireDate = expireDate;
+    }
+
+    public decimal Amount { get; private set; }
+
+    public DateTime ExpireDate { get; private set; }
+
+    public bool IsValid()
+    {
+        return DateTime.Compare(DateTime.Now, ExpireDate) < 0;
+    }
+
+    public decimal Value()
+    {
+        if (IsValid())
+            return Amount;
+        else
+            return 0;
+    }
+}
