b8d211169dd678fe143e220a8d8596b75aa37591
Lincoml Rufino <linconldasilvarufino@gmail.com>
2022-08-04 21:56:44 -0300

    Testes da entidade Order

--- Store.Tests/Domain/OrderTests.cs
+++ Store.Tests/Domain/OrderTests.cs
@@ -40,49 +40,81 @@ public class OrderTests
     [Trait("Category", "Domain")]
     public void Dado_um_pagamento_do_pedido_seu_status_deve_ser_aguardando_entrega()
     {
-        Assert.True(false);
+        var order = new Order(customer, 0, discount);
+
+        order.AddItem(product, 2);
+
+        order.Pay(10);
+
+        Assert.Equal(EOrderStatus.WaitingDelivery, order.Status);
     }
 
     [Fact]
     [Trait("Category", "Domain")]
     public void Dado_um_pedido_cancelado_seu_status_deve_ser_cancelado()
     {
-        Assert.True(false);
+        var order = new Order(customer, 0, discount);
+
+        order.Cancel();
+
+        Assert.Equal(EOrderStatus.Canceled, order.Status);
     }
 
     [Fact]
     [Trait("Category", "Domain")]
     public void Dado_um_novo_item_sem_produto_o_mesmo_nao_deve_ser_adicionado()
     {
-        Assert.True(false);
+        var order = new Order(customer, 0, discount);
+
+        order.AddItem(null, 2);
+
+        Assert.Empty(order.Items);
     }
 
     [Fact]
     [Trait("Category", "Domain")]
     public void Dado_um_novo_item_com_quantidade_zero_ou_menor_o_mesmo_nao_deve_ser_adicionado()
     {
-        Assert.True(false);
+        var order = new Order(customer, 0, discount);
+
+        order.AddItem(product, 0);
+        order.AddItem(product, -2);
+
+        Assert.Empty(order.Items);
     }
 
     [Fact]
     [Trait("Category", "Domain")]
     public void Dado_um_novo_pedido_valido_seu_total_deve_ser_50()
     {
-        Assert.True(false);
+        var order = new Order(customer, 10, discount);
+
+        order.AddItem(product, 5);
+
+        Assert.Equal(50, order.Total());
     }
 
     [Fact]
     [Trait("Category", "Domain")]
     public void Dado_um_desconto_expirado_o_valor_do_pedido_deve_ser_60()
     {
-        Assert.True(false);
+        var expiredDiscount = new Discount(5,DateTime.Now.AddDays(-5));
+        var order = new Order(customer, 10, expiredDiscount);
+
+        order.AddItem(product, 5);
+
+        Assert.Equal(60, order.Total());
     }
 
     [Fact]
     [Trait("Category", "Domain")]
     public void Dado_um_desconto_invalido_o_valor_do_pedido_deve_ser_60()
     {
-        Assert.True(false);
+        var order = new Order(customer, 10, null);
+
+        order.AddItem(product, 5);
+
+        Assert.Equal(60, order.Total());
     }
 
     [Fact]
