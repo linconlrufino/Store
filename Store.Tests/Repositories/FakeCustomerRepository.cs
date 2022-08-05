6f0e84906ba31e5f65d7a0d7a95e3c08ddeb9f3f
Lincoml Rufino <linconldasilvarufino@gmail.com>
2022-08-04 22:26:31 -0300

    creating FakeCustomerRepository

---
+++ Store.Tests/Repositories/FakeCustomerRepository.cs
@@ -0,0 +1,16 @@
+﻿using Store.Domain.Entities;
+using Store.Domain.Repositories;
+
+namespace Store.Tests.Repositories
+{
+    public class FakeCustomerRepository : ICustumerRepository
+    {
+        public Customer Get(string document)
+        {
+            if (document == "12345678911")
+                return new Customer("Tolstoi", "tolstoi@tst.com");
+
+            return null;
+        }
+    }
+}
