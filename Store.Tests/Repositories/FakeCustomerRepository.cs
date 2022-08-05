35c6be91dc129b59dc9215b40c608b86314adb6b
Lincoml Rufino <linconldasilvarufino@gmail.com>
2022-08-04 22:35:22 -0300

    creating FakeProductRepository , FakeDeliveryFeeRepository,FakeDiscountRepository , FakeProductRepository , FakeCusomerRepository

--- Store.Tests/Repositories/FakeCustomerRepository.cs
+++ Store.Tests/Repositories/FakeCustomerRepository.cs
@@ -1,16 +1,15 @@
 ﻿using Store.Domain.Entities;
 using Store.Domain.Repositories;
 
-namespace Store.Tests.Repositories
-{
-    public class FakeCustomerRepository : ICustumerRepository
-    {
-        public Customer Get(string document)
-        {
-            if (document == "12345678911")
-                return new Customer("Tolstoi", "tolstoi@tst.com");
+namespace Store.Tests.Repositories;
 
-            return null;
-        }
+public class FakeCustomerRepository : ICustumerRepository
+{
+    public Customer Get(string document)
+    {
+        if (document == "12345678911")
+            return new Customer("Tolstoi", "tolstoi@tst.com");
+
+        return null;
     }
 }
---
+++ Store.Tests/Repositories/FakeDiscountRepository.cs
@@ -0,0 +1,18 @@
+﻿using Store.Domain.Entities;
+using Store.Domain.Repositories;
+
+namespace Store.Tests.Repositories;
+
+public class FakeDiscountRepository : IDiscountRepository
+{
+    public Discount Get(string code)
+    {
+        if (code == "10OFF")
+            return new Discount(10, DateTime.Now.AddDays(5));
+
+        if (code == "20OFF")
+            return new Discount(20, DateTime.Now.AddDays(-5));
+
+        return null;
+    }
+}
